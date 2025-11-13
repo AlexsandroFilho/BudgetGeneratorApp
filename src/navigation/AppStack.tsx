659ce9948bac28a3f957e1328a8c08e999a0fc92
Hrafa <hrafa@development.local>
2025-11-13 13:22:12 -0300

    feat: Atualiza AppStack.tsx

--- src/navigation/AppStack.tsx
+++ src/navigation/AppStack.tsx
@@ -1,19 +1,9 @@
 // src/navigation/AppStack.tsx
 
 import React from 'react';
-import { View, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/Home/HomeScreen';
 import { OrcamentosScreen } from '../screens/Orcamento/Orcamentos';
-
-// Componente HomeScreen local criado para evitar import inexistente.
-// Substitua por sua implementação real em ../screens/Home/HomeScreen quando adicionar o arquivo.
-export const HomeScreen: React.FC = () => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
-    </View>
-  );
-};
 
 // Tipagem para as rotas do App Logado
 export type AppStackParamList = {
