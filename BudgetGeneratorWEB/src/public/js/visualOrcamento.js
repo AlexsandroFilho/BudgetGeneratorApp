d1f99debf597cd1cd07224d297b5554aca77c1ec
LukasIanni <ianni.lukas5@gmail.com>
2025-11-20 10:06:31 -0300

    chore: adicionar pastas de backend ao gitignore (BudgetGeneratorWEB, BudgetGenerator-V2, backend)

--- BudgetGeneratorWEB/src/public/js/visualOrcamento.js
+++
@@ -1,6 +0,0 @@
-const container = document.getElementById('orcamento-dados');
-if (dados) {
-  container.innerHTML = `<pre class="slide-in">${JSON.stringify(dados, null, 2)}</pre>`;
-} else {
-  container.innerHTML = "<p class='slide-in'>Nenhum orçamento encontrado.</p>";
-}
